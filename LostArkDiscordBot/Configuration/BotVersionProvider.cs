730db82f9ca83a1a4049eb84cf3d9253b19ad707
Wiktor Napierała <wiktor.napierala@outlook.com>
2022-03-12 00:21:30 +0100

    Bot version update

--- LostArkDiscordBot/Configuration/BotVersionProvider.cs
+++ LostArkDiscordBot/Configuration/BotVersionProvider.cs
@@ -8,16 +8,29 @@ public class BotVersionProvider : IBotVersionProvider
     public BotVersionProvider(IHostEnvironment hostEnvironment)
     {
         var assembly = Assembly.GetEntryAssembly();
-        var attribute = assembly?.GetCustomAttribute<AssemblyFileVersionAttribute>();
 
-        if (attribute != null)
-            BotVersion = attribute.Version;
-
-        if (hostEnvironment.IsDevelopment())
-            BotVersion = $"{BotVersion}-local";
+        if (assembly is not null)
+            BotVersion = GetBotVersion(assembly, hostEnvironment);
     }
 
     public string BotVersion { get; } = "1.0";
+
+    private string GetBotVersion(Assembly assembly, IHostEnvironment environment)
+    {
+        if (environment.IsProduction())
+        {
+            var version = assembly.GetName().Version;
+            if (version is not null)
+                return version.ToString();
+        }
+
+        var fileAttribute = assembly.GetCustomAttribute<AssemblyFileVersionAttribute>();
+        if (fileAttribute is null)
+            return BotVersion;
+
+        var fileVersion = fileAttribute.Version;
+        return environment.IsDevelopment() ? $"{fileVersion}-local" : fileVersion;
+    }
 }
 
 public interface IBotVersionProvider
